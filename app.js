e13d36b8ad404a0c78523041bfe1f5fe1379289b
ninja-mayank <112952488+ninja-mayank@users.noreply.github.com>
2023-06-15 23:27:50 +0530

    get,post and delete

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ mongoose.connect(dbURI).then(() => {
 }).catch((err) => console.log(err));
 
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
 app.use(morgan('dev'));
 
 // app.get('/add-blog',(req,res) => {
@@ -57,8 +58,35 @@ app.get('/blogs',(req,res) => {
         .catch(err => console.log(err));
 })
 
+app.post('/blogs',(req,res) => {
+    const blog = new Blog(req.body);
+    blog.save()
+        .then((result) => {
+            res.redirect('/blogs');
+        })
+        .catch(err => console.log(err));
+})
+
 app.get('/blogs/create',(req,res) => {
     res.render('create', {title : 'Create a new blog'});
+})
+
+app.get('/blogs/:id',(req,res) => {
+    const id = req.params.id;
+    Blog.findById(id)
+        .then((result) => {
+            res.render('details',{blog : result, title : 'Blog details'});
+        })
+        .catch(err => console.log(err));
+})
+
+app.delete('/blogs/:id',(req,res) => {
+    const id = req.params.id;
+    Blog.findByIdAndDelete(id)
+        .then(result => {
+            res.json({redirect : '/blogs'});
+        })
+        .catch(err => console.log(err));
 })
 
 //404 page
