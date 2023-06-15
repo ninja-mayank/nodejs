8e7920cac025a716f815f01140efc7375f56adec
ninja-mayank <112952488+ninja-mayank@users.noreply.github.com>
2023-06-16 01:06:44 +0530

    express routers and MVC

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog');
+const blogRoutes = require('./routes/blogRoutes');
 
 //register view engine
 app.set('view engine','ejs');
@@ -50,45 +50,7 @@ app.get('/about',(req,res) => {
     res.render('about', {title : 'About'});
 });
 
-app.get('/blogs',(req,res) => {
-    Blog.find()
-        .then((result) => {
-            res.render('index', {title: 'All blogs', blogs : result});
-        })
-        .catch(err => console.log(err));
-})
-
-app.post('/blogs',(req,res) => {
-    const blog = new Blog(req.body);
-    blog.save()
-        .then((result) => {
-            res.redirect('/blogs');
-        })
-        .catch(err => console.log(err));
-})
-
-app.get('/blogs/create',(req,res) => {
-    res.render('create', {title : 'Create a new blog'});
-})
-
-app.get('/blogs/:id',(req,res) => {
-    const id = req.params.id;
-    Blog.findById(id)
-        .then((result) => {
-            res.render('details',{blog : result, title : 'Blog details'});
-        })
-        .catch(err => console.log(err));
-})
-
-app.delete('/blogs/:id',(req,res) => {
-    const id = req.params.id;
-    Blog.findByIdAndDelete(id)
-        .then(result => {
-            res.json({redirect : '/blogs'});
-        })
-        .catch(err => console.log(err));
-})
-
+app.use('/blogs',blogRoutes)
 //404 page
 app.use((req,res) => {
     res.status(404).render('404', {title : '404'});
