94965821516c0a00bae89a744c8637ae3d18738d
ninja-mayank <112952488+ninja-mayank@users.noreply.github.com>
2023-06-15 19:54:23 +0530

    MongoDB

--- app.js
+++ app.js
@@ -1,29 +1,61 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+
 //register view engine
 app.set('view engine','ejs');
 
-//listen for request
-app.listen(3000);
+//connect to mongodb
+const dbURI = 'mongodb+srv://ninjamayank:Mayank%40123@cluster0.gflw2z1.mongodb.net/node-crash?retryWrites=true&w=majority';
+mongoose.connect(dbURI).then(() => {
+    app.listen(3000);
+}).catch((err) => console.log(err));
 
 app.use(express.static('public'));
 app.use(morgan('dev'));
 
+// app.get('/add-blog',(req,res) => {
+//     const blog = new Blog({
+//         title : 'new blog 2',
+//         snippet : 'About my blog',
+//         body : 'More about my blog'
+//     });
+//     blog.save()
+//         .then((result) => {
+//             res.send(result);
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//         })
+// })
+
+// app.get('/all-blogs',(req,res) => {
+//     Blog.find()
+//         .then((result) => {
+//             res.send(result);
+//         })
+//         .catch(err => console.log(err));
+// })
+
 app.get('/',(req,res) => {
     //res.send('<p>Home Page</p>')
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    ];
-    res.render('index', {title : 'Home', blogs});
+    res.redirect('/blogs');
 });
 
 app.get('/about',(req,res) => {
     //res.send('<p>About Page</p>')
     res.render('about', {title : 'About'});
 });
+
+app.get('/blogs',(req,res) => {
+    Blog.find()
+        .then((result) => {
+            res.render('index', {title: 'All blogs', blogs : result});
+        })
+        .catch(err => console.log(err));
+})
 
 app.get('/blogs/create',(req,res) => {
     res.render('create', {title : 'Create a new blog'});
